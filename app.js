b28ba87515c822c937209c093bc5c0419d78ef07
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-03-02 13:29:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var myCharacteristic;
     const btn = document.getElementById("btn");
-    const text = document.getElementById("batt");
+    const text = document.getElementById("batt_level");
 
   btn.addEventListener("click", async event =>{
         startButton();
